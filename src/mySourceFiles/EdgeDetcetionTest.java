5b958ba6feb26267f330d1d932bb7303c19b42f2
JoeXie <imxsz@qq.com>
2014-04-18 20:33:42 +0800

    写入灰度化代码

--- src/mySourceFiles/EdgeDetcetionTest.java
+++ src/mySourceFiles/EdgeDetcetionTest.java
@@ -1,14 +1,14 @@
 package mySourceFiles;
 
+import java.awt.Component;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.image.MemoryImageSource;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-
-import bmpIO.BmpImage;
-import bmpIO.BmpReader;
-import bmpIO.BmpWriter;
 
 
 public class EdgeDetcetionTest 
@@ -21,30 +21,76 @@ public class EdgeDetcetionTest
 	 * ********************************************/
 	public static void main(String[] args) throws IOException
 	{
-		//导入图片Demo.bmp。
+		/**********************************************
+		 * 导入图片Demo.bmp。保存在bmp中。
+		 **********************************************/
 		InputStream in = new FileInputStream("Demo.bmp");
-		BmpImage bmp;
 		
+		BMPDecoder bmp = new BMPDecoder();
 		try 
 		{
-			bmp = BmpReader.read(in);
+			bmp.read(in);
 		} 
-		finally 
+		catch (Exception e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally
 		{
 			in.close();
 		}
 		
 		
-		//调用BeBinary类进行二值化处理。
+		/*********************************************
+		 * 进行灰度化处理。
+		 *********************************************/
+		BMPDecoder newbmp = bmp;
+		int m_b, m_g, m_r, m_gray, m_graydata;
 		
-		//调用GetGrainEdge类进行边缘跟踪。
+		for(int i = 0; i < newbmp.height ; i ++)
+		{
+			for(int j = 0; j < newbmp.width; j ++)
+			{
+				m_r = (newbmp.intData[i * newbmp.width + j] & 0x00FF0000) >> 16; //右移16位
+				m_b = newbmp.intData[i * newbmp.width + j] & 0x000000FF;
+				m_g = (newbmp.intData[i * newbmp.width + j] & 0x0000FF00) >> 8; //右移8位
+				m_gray = (30 * m_r + 59 * m_g + 11 * m_b) / 100;
+				m_graydata = m_gray | (m_gray << 8) | (m_gray << 16) | 0xFF000000;
+				newbmp.intData[i * newbmp.width + j] = m_graydata;
+			}
+		}
 		
-		//保存图像到Demo2.bmp。
+		
+		MemoryImageSource mis= newbmp.makeImageSource(); //图像生成类？？？？？？？？？？？这个类和方法有待理解
+		//MyFrame.BMPDecoder= newbmp;
+		Image image= Toolkit.getDefaultToolkit().createImage(mis); //创建图像
+		//MyFrame.canvas.myImage= image;
+		//MyFrame.canvas.repaint();
+		
+		/**************************************************
+		 * 二值化处理。
+		 **************************************************/
+		
+		
+		
+		
+		
+		/**************************************************
+		 * 调用GetGrainEdge类进行边缘跟踪。
+		 **************************************************/
+		
+		
+		/*************************************************
+		 * 将bmp中的图片保存图像到Demo2.bmp。
+		 *************************************************/
+		
 		OutputStream out = new FileOutputStream("Demo2.bmp");
 		
 		try 
 		{
-			BmpWriter.write(out, bmp);
+			Image img = Component.createImage(bmp.makeImageSource());
+			//makeImageSource()不知道怎么使用
 		} 
 		finally 
 		{
